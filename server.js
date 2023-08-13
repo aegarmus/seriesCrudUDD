03c1a55d4b3dd294477d6771c5d9c10f8c097584
aegarmus <aegarmus@gmail.com>
2023-08-12 21:30:46 -0400

    feat agregamos funciones CRUD

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createSerie } from './src/controllers/controlsSeries.js';
+import { createSerie, deleteSerie, updateSerie } from './src/controllers/controlsSeries.js';
 
 
 
@@ -10,6 +10,29 @@ const PORT = 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/series', async (req, res) => {
+    try {
+  
+      const series = await getAllSeries();
+      res.status(200).json({ message: 'Series leídas con éxito', series });
+  
+    } catch (error) {
+      res.status(404).json({ message: 'Series no encontradas', error});
+    }
+  });
+  
+  app.get('/series/id/:id', async (req, res) => {
+    try {
+  
+      const id = parseInt(req.params.id);
+      const serie = await getSerieById(id);
+      console.log(serie);
+      res.status(200).json({ message: `Serie ${id} encontrada`, serie });
+  
+    } catch (error) {
+      res.status(404).json({ message: 'Serie no encontrada', error});
+    }
+  });
 
 app.post('/series', async (req, res) => {
     //req = request = petición del cliente y res = response = respuesta del servidor
@@ -27,6 +50,26 @@ app.post('/series', async (req, res) => {
     }
 })
 
+app.put('/series/:id', async (req, res) => {
+    try {
+        const id  = parseInt(req.params.id)
+        const {name, gender, released, season, author} = req.body
+        const serie = await updateSerie(id, name, gender, released, season, author)
+        res.status(203).json({message: 'Serie creada con éxito', status: 203, serie})
+    } catch (error) {
+        res.status(500).json({message: 'Lo sentimos! no pudimos actualizar la serie', status: 500, error})
+    }
+});
+
+app.delete('/series/:id', async(req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const series = await deleteSerie(id)
+        res.status(202).json({message: 'Serie eliminada con éxito', status: 202, series})
+    } catch (error) {
+        res.status(500).json({message: 'Lo sentimos! no pudimos actualizar la serie', status: 500, error})
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}` )
--- src/controllers/controlsSeries.js
+++ src/controllers/controlsSeries.js
@@ -1,9 +1,21 @@
 import { Series } from '../models/Series.js'
 import { readSeries, writeSeries } from '../utils/fsFunction.js'
 
-
-
-
+export const getAllSeries = async () => {
+    const series = await readSeries();
+    return series;
+  }
+  
+export const getSerieById = async (id) => {
+    const data = await readSeries();
+    // find devuelve sólo la primera coincidencia que encuentra
+    const series = data.find( serie => {
+      if(serie.id === id) {
+        return serie;
+      }
+    } );
+    return series;
+}
 
 export const createSerie = async(id, name, gender, released, season, author) => {
 
@@ -14,3 +26,20 @@ export const createSerie = async(id, name, gender, released, season, author) => 
     await writeSeries(series)
     return series
 } 
+
+export const updateSerie = async (id, name, gender, released, season, author) => {
+    const serie = new Series(id, name, gender, released, season, author)
+    const series = await readSeries()
+    const serieIndex = series.findIndex(index => index.id === id)
+    series[serieIndex] = serie
+
+    await writeSeries(series)
+    return series
+}
+
+export const deleteSerie = async(id) => {
+    const series = await readSeries();
+    const filterSeries = series.filter(serie => serie.id != id)
+    await writeSeries(filterSeries)
+    return filterSeries
+}
